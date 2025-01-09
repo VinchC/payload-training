11a2d468f35a81a5180daa87002050cdbd9265b4
Vincent C <vinch.cm@gmail.com>
2025-01-09 16:47:47 +0100

    CarPriceLabel fixed with payloadcms package installed

--- src/components/CarPriceLabel.tsx
+++ src/components/CarPriceLabel.tsx
@@ -1,9 +1,40 @@
 'use client'
 
 import React from 'react'
+import { useFormFields } from '@payloadcms/ui'
+import { Car } from '@/payload-types'
 
 const CarPriceLabel: React.FC = () => {
-  return <p>Hello</p>
+  // return <p>Hello bbbbbbbbbbbbbbb</p>
+  const carID = useFormFields(([fields]) => fields?.car?.value)
+  const [car, setCar] = React.useState<Car>()
+
+  React.useEffect(() => {
+    if (car?.id !== carID) {
+      const fetchCar = async () => {
+        try {
+          const carResult: Car = await fetch(`/api/cars/${carID}?depth=0`).then((res) => res.json())
+
+          if (carResult) {
+            setCar(carResult)
+          }
+        } catch (e) {
+          console.error(e)
+        }
+      }
+      fetchCar()
+    }
+  }, [car, carID])
+
+  if (carID && typeof car === 'undefined') {
+    return <span>Loading...</span>
+  }
+
+  if (!carID || !car) {
+    return <span>No car selected</span>
+  }
+
+  return <span>{car.title}</span>
 }
 
 export default CarPriceLabel
